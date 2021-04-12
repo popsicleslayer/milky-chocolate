a8052af949415833ee91a91f4e81ff7b5db47825
popsicleslayer <70440489+popsicleslayer@users.noreply.github.com>
2021-04-12 15:01:16 +0200

    Created models: Category, Institution.

--- charity_lab/charity_donation_app/models.py
+++ charity_lab/charity_donation_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=256)
+
+class Institution(models.Model):
+    FOUNDATION_TYPES = [
+        (0, 'Fundacja'),
+        (1, 'Organizacja pozarządowa'),
+        (2, 'Zbiórka lokalna')
+    ]
+    name = models.CharField(max_length=256)
+    description = models.TextField()
+    type = models.IntegerField(choices=FOUNDATION_TYPES, default=0)
+    categories = models.ManyToManyField(Category)
