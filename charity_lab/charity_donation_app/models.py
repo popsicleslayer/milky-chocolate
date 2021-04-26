69c2261ecd82da3e480a72003ebc47d07f9341a6
popsicleslayer <70440489+popsicleslayer@users.noreply.github.com>
2021-04-26 11:23:58 +0200

    Added UserAdmin to admin.py
    Added UserAdmin to admin.py
    Reorganized models.py

--- charity_lab/charity_donation_app/models.py
+++ charity_lab/charity_donation_app/models.py
@@ -1,7 +1,56 @@
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+# from charity_lab.settings import AUTH_USER_MODEL as MyUser
+
+
+class UserManager(BaseUserManager):
+    """Model manager for User model without username field"""
+
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        """Create and save a User with a given email and password"""
+        if not email:
+            raise ValueError('Email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create and save a regular User with a given email and password"""
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        """Create and save a Superuser with a given email and password"""
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True')
+
+        return self._create_user(email, password, **extra_fields)
+
+
+class User(AbstractUser):
+    """User model"""
+    username = None
+    USERNAME_FIELD = 'email'
+    email = models.EmailField(_('email address'), unique=True)
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
 
 class Category(models.Model):
     name = models.CharField(max_length=256)
+
 
 class Institution(models.Model):
     FOUNDATION_TYPES = [
@@ -13,3 +62,21 @@ class Institution(models.Model):
     description = models.TextField()
     type = models.IntegerField(choices=FOUNDATION_TYPES, default=0)
     categories = models.ManyToManyField(Category)
+
+
+    def __str__(self):
+        return self.name
+
+
+class Donation(models.Model):
+    quantity = models.IntegerField()
+    categories = models.ManyToManyField(Category)
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    address = models.CharField(max_length=256)
+    phone_number = models.IntegerField()
+    city = models.CharField(max_length=64)
+    zip_code = models.CharField(max_length=10)
+    pick_up_date = models.DateField()
+    pick_up_time = models.TimeField()
+    pick_up_comment = models.CharField(max_length=256)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
